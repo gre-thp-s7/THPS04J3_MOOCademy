83b7616d460615a8b3213a571e18ed3e061bd188
dendiflet <bretin.iannis@gmail.com>
2019-01-30 22:24:52 +0100

    ajout de 6lignes dans le seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+course1 = Course.create!(title: "sex", description: "all about practice")
+course2 = Course.create!(title: "bicycle", description: "all about pédale")
+
+lesson1 = Lesson.new(title: "how to eat pussy", body: "you got hand, mouse, ears, and sometimes eyes, use them, combine them !", course_id: 1)
+lesson1.save
+lesson2 = Lesson.create!(title: "why we says loosing virginity", body: "everybody exactly know where it had gone !", course_id: 1)
+lesson3= Lesson.new(title: "how to choose your bycicle", body: "all about money", course_id: 2)
+lesson3.save
+lesson4 = Lesson.create!(title: "gear does matter?", body: "all about pleasure", course_id: 2)
+
+
+
+
